0ddefee0d6153673dfc2fda0d53f633538eef3b5
jojocys <yuyilobay@gmail.com>
2020-07-23 15:40:23 +0800

    fix build path

--- examples/index.ts
+++ examples/index.ts
@@ -21,3 +21,32 @@ progressEle.style.width = '0%';
   ).then(data => console.log(data));
   // FileReader.csvReader(file).then(data => console.log(data));
 };
+
+fetch("http://localhost:1234/").then((response) => {
+  const reader = response.body.getReader();
+  const stream = new ReadableStream({
+    start(controller) {
+      // 下面的函数处理每个数据块
+      function push() {
+        // "done"是一个布尔型，"value"是一个Unit8Array
+        reader.read().then(({ done, value }) => {
+          console.log(value);
+          // 判断是否还有可读的数据？
+          if (done) {
+            // 告诉浏览器已经结束数据发送。
+            controller.close();
+            return;
+          }
+
+          // 取得数据并将它通过controller发送给浏览器。
+          controller.enqueue(value);
+          push();
+        });
+      }
+
+      push();
+    },
+  });
+
+  return new Response(stream, { headers: { "Content-Type": "text/html" } });
+});
